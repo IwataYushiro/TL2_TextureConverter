17da92221f86b6b489da026afe68d16d59360478
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2023-12-18 11:43:13 +0900

    dds出力完了

--- Header/TextureConverter.h
+++ Header/TextureConverter.h
@@ -10,9 +10,24 @@ public:
 	void ConvertTextureWICToDDS(const std::string& filePath);
 
 private:
+	//画像の情報
+	DirectX::TexMetadata metadata_;
+	//画像イメージのコンテナ
+	DirectX::ScratchImage scratchImage_;
+	//ディレクトリパス
+	std::wstring directoryPath_;
+	//ファイル名
+	std::wstring fileName_;
+	//ファイル拡張子
+	std::wstring fileExt_;
+
+private:
 	//テクスチャファイル読み込み(ファイルパス)
 	void LoadWICTextureFromFile(const std::string& filePath);
 	//マルチバイト文字列をワイド文字列に変換(マルチバイト文字列)
 	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
-
+	//フォルダパスとファイル名を分離
+	void SeparateFilePath(const std::wstring& filePath);
+	//DDSテクスチャとしてファイル書き出し
+	void SaveDDSTextureToFile();
 };
